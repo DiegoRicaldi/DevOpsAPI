76b390fb5302e05b642a0975a697aa34e6780511
ricaldimd15@gmail.com <ricaldimd15@gmail.com>
2024-10-27 20:15:44 -0400

    pruebas

--- tests/comidas.test.js
+++ tests/comidas.test.js
@@ -1,21 +1,22 @@
 process.env.NEW_RELIC_NO_CONFIG_FILE = 'true'; 
+//evitar a new relic
 const request = require('supertest');
-const app = require('../index'); // Asegúrate de que la ruta a tu archivo `index.js` sea correcta
+const app = require('../index'); 
 
 describe('Comidas API Tests', () => {
     it('GET /api/comidas - should return all comidas', async () => {
         const response = await request(app).get('/api/comidas');
         
-        // Verificamos que el código de estado sea 200
+      
         expect(response.statusCode).toBe(200);
         
-        // Verificamos que la respuesta sea un arreglo
+      
         expect(Array.isArray(response.body)).toBe(true);
         
-        // Verificamos que la longitud del arreglo sea la esperada (en este caso, 3)
+       
         expect(response.body.length).toBe(3);
 
-        // Verificamos que el contenido del arreglo sea el esperado
+       
         expect(response.body).toEqual(expect.arrayContaining([
             { idcomida: 1, nombre: 'alitas', costo: 1000 },
             { idcomida: 2, nombre: 'pizza', costo: 4000 },
@@ -23,3 +24,5 @@ describe('Comidas API Tests', () => {
         ]));
     });
 });
+
+

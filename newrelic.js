5454d4b5371854151e1337cda38429a58928b04e
ricaldimd15@gmail.com <ricaldimd15@gmail.com>
2024-10-29 15:24:35 -0400

    secretos

--- newrelic.js
+++ newrelic.js
@@ -1,16 +1,12 @@
 'use strict';
-
+require('dotenv').config(); 
 
 exports.config = {
-
   app_name: ['devops'],
-
-  license_key: '6786124f81127b18d3590b41755b4233FFFFNRAL',
+  license_key: process.env.NEW_RELIC_LICENSE_KEY, 
   logging: {
-   
     level: 'info',
   },
- 
   transaction_tracer: {
     enabled: true,
   },

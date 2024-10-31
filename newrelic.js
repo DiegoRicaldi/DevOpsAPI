91b41a226ea29421b60a9c15f9c6100579347b62
ricaldimd15@gmail.com <ricaldimd15@gmail.com>
2024-10-31 12:55:26 -0400

    secretos

--- newrelic.js
+++ newrelic.js
@@ -2,8 +2,7 @@
 
 exports.config = {
   app_name: ['devops-monitoreo'], 
-  //license_key: 'f1f3b9bc5dcfdd80d2ba8b910bb0c92fFFFFNRAL',
-  license_key: process.env.NEW_RELIC_LICENSE_KEY,
+  license_key: 'f1f3b9bc5dcfdd80d2ba8b910bb0c92fFFFFNRAL',
   logging: {
     level: 'info'
   },
